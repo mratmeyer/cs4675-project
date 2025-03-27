95b60218a4eb8752c07cbc6a37847229de08e0b0
alina27p <alina27p@gmail.com>
2025-03-27 10:45:22 -0400

    keeping input in search results on second page

--- frontend/src/search-bar.jsx
+++ frontend/src/search-bar.jsx
@@ -1,18 +1,33 @@
-import {useNavigate} from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import './search-bar.css'
 
 function SearchBar({results}) {
   const navigate = useNavigate()
+  const [searchParams] = useSearchParams();
+  const initialQuery = searchParams.get("query") || "";
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    setQuery(initialQuery);
+  }, [initialQuery]);
 
   const handleSearchClick = (event) => {
     event.preventDefault();
-    navigate('/results')
+    if (query.trim() !== '') {
+      navigate(`/results?query=${encodeURIComponent(query)}`);
+    }
   }
 
     return (
         <div className={`search-bar-response${results ? '-results': ''}`}>
         <form onSubmit={handleSearchClick}>
-          <input type="text" placeholder="Ask Buzz anything..." name="search" />
+          <input type="text" 
+            placeholder="Ask Buzz anything..." 
+            name="search" 
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            />
           <button>
             Search
           </button>
