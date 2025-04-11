bd616ce77776d90d108af2e89d2158bb00907339
cpriest7 <ctpriest12@earthlink.net>
2025-04-11 11:00:06 -0400

    updated ui to single column of blocks

--- frontend/src/search-result.jsx
+++ frontend/src/search-result.jsx
@@ -12,15 +12,18 @@ function SearchResults({ data }) {
       {data.summary && <p><strong>{data.summary}</strong></p>}
 
       {data.results && data.results.length > 0 ? (
-        <ul>
+        <ul className="response-list">
           {data.results.map((event, index) => (
-            <li key={index} className="event-card">
+            <li key={index} className="response-list-item">
+              <a href={event.calendar_link} target="_blank" rel="noopener noreferrer" 
+              className="block no-underline text-inherit"> 
               <h3>{event.description}</h3>
               <p><strong>Date:</strong> {event.date}</p>
               <p><strong>Time:</strong> {event.time}</p>
               <p><strong>Department:</strong> {event.department}</p>
               <a href={event.calendar_link} target="_blank" rel="noopener noreferrer">
                 View Event Details
+              </a>
               </a>
             </li>
           ))}
